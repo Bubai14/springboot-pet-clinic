6437205b68615f5fa7051a0e24a18dc2434843b6
bubaibal <bubaibal@localhost>
2021-03-14 11:56:58 +0530

    Adding Index page and controller. Closes #7.

---
+++ petclinic/petclinicweb/src/main/java/com/playground/spring/petclinic/controller/IndexController.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.playground.spring.petclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author bubaibal
+ *
+ */
+@Controller
+public class IndexController {
+	
+	@GetMapping({"", "/", "index","index.html"})
+	public String index() {
+		return "index";
+	}
+
+}
