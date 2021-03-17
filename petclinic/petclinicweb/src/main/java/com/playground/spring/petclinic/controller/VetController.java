89db2ffa8c48cb68618e35197e8274fa3eb5f38e
bubaibal <bubaibal@localhost>
2021-03-17 20:31:26 +0530

    Adding list of vets. Closes #14.

--- petclinic/petclinicweb/src/main/java/com/playground/spring/petclinic/controller/VetController.java
+++ petclinic/petclinicweb/src/main/java/com/playground/spring/petclinic/controller/VetController.java
@@ -4,7 +4,10 @@
 package com.playground.spring.petclinic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.playground.spring.petclinic.service.VetService;
 
 /**
  * @author bubaibal
@@ -12,9 +15,24 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class VetController {
+	
+	private final VetService vetService;
+	
+
+	/**
+	 * @param vetService
+	 */
+	public VetController(VetService vetService) {
+		this.vetService = vetService;
+	}
+
+
 
 	@GetMapping({"/vets", "/vets/index", "/vets/index.html"})
-	public String listVets() {
+	public String listVets(Model model) {
+		
+		model.addAttribute("vets", vetService.findAll());
+		
 		return "vets/index";
 	}
 }
