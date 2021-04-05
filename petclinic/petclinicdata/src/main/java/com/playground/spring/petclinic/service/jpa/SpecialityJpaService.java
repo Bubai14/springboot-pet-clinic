7c77b8f416130b2d9ad730c55e387c36922e2321
bubaibal <bubaibal@localhost>
2021-04-05 23:29:48 +0530

    Adding Vet Speciality Jpa service. Closes #43.

---
+++ petclinic/petclinicdata/src/main/java/com/playground/spring/petclinic/service/jpa/SpecialityJpaService.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package com.playground.spring.petclinic.service.jpa;
+
+import java.util.Collections;
+import java.util.Set;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import com.playground.spring.petclinic.model.Speciality;
+import com.playground.spring.petclinic.repository.SpecialityRepository;
+import com.playground.spring.petclinic.service.SpecialityService;
+
+/**
+ * @author bubaibal
+ *
+ */
+@Service
+@Profile(value = "jpa")
+public class SpecialityJpaService implements SpecialityService {
+	
+	private final SpecialityRepository specialityRepository;
+
+	/**
+	 * @param specialityRepository
+	 */
+	public SpecialityJpaService(SpecialityRepository specialityRepository) {
+		this.specialityRepository = specialityRepository;
+	}
+
+	@Override
+	public Set<Speciality> findAll() {
+		Set<Speciality> specialities = Collections.emptySet();
+		specialityRepository.findAll().forEach(specialities::add);
+		return specialities;
+	}
+
+	@Override
+	public Speciality findById(Long id) {
+		return specialityRepository.findById(id).orElse(null);
+	}
+
+	@Override
+	public Speciality save(Speciality t) {
+		return specialityRepository.save(t);
+	}
+
+	@Override
+	public void delete(Speciality t) {
+		specialityRepository.delete(t);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		specialityRepository.deleteById(id);
+	}
+
+}
