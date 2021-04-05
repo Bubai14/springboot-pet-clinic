299589e3dc68f48e0dc0cfe8662dc006bd7b0963
bubaibal <bubaibal@localhost>
2021-04-05 21:34:41 +0530

    Adding the base entity. Closes #34.

--- petclinic/petclinicdata/src/main/java/com/playground/spring/petclinic/model/BaseEntity.java
+++ petclinic/petclinicdata/src/main/java/com/playground/spring/petclinic/model/BaseEntity.java
@@ -5,17 +5,29 @@ package com.playground.spring.petclinic.model;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
 /**
+ * Base class for all the JPA entities. The class is annotated with
+ * {@code @MappedSuperclass} to indicate JPA that this class will used as a base
+ * class for all the JPA entities.
+ * 
  * @author bubaibal
  *
  */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -7662069729198382751L;
-
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	/**
