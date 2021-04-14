0e6970c67e1b045631101f2626c20ef94234a325
bubaibal <bubaibal@localhost>
2021-04-14 20:11:08 +0530

    Adding Visit Map Service. Closes #47.

---
+++ petclinic/petclinicdata/src/main/java/com/playground/spring/petclinic/service/VisitService.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.playground.spring.petclinic.service;
+
+import com.playground.spring.petclinic.model.Visit;
+
+/**
+ * @author bubaibal
+ *
+ */
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
