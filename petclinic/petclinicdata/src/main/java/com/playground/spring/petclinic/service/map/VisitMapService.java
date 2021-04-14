e1127d0acbcccb2b9e5ddd98de928cb5da9875db
bubaibal <bubaibal@localhost>
2021-04-14 20:24:51 +0530

    Adding data load of visit on start up. Closes #49.

--- petclinic/petclinicdata/src/main/java/com/playground/spring/petclinic/service/map/VisitMapService.java
+++ petclinic/petclinicdata/src/main/java/com/playground/spring/petclinic/service/map/VisitMapService.java
@@ -5,6 +5,8 @@ package com.playground.spring.petclinic.service.map;
 
 import java.util.Set;
 
+import org.springframework.stereotype.Service;
+
 import com.playground.spring.petclinic.model.Visit;
 import com.playground.spring.petclinic.service.VisitService;
 
@@ -12,6 +14,7 @@ import com.playground.spring.petclinic.service.VisitService;
  * @author bubaibal
  *
  */
+@Service
 public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
 
 	@Override
--- petclinic/petclinicweb/src/main/java/com/playground/spring/petclinic/bootstrap/DataLoader.java
+++ petclinic/petclinicweb/src/main/java/com/playground/spring/petclinic/bootstrap/DataLoader.java
@@ -13,10 +13,12 @@ import com.playground.spring.petclinic.model.Pet;
 import com.playground.spring.petclinic.model.PetType;
 import com.playground.spring.petclinic.model.Speciality;
 import com.playground.spring.petclinic.model.Vet;
+import com.playground.spring.petclinic.model.Visit;
 import com.playground.spring.petclinic.service.OwnerService;
 import com.playground.spring.petclinic.service.PetTypeService;
 import com.playground.spring.petclinic.service.SpecialityService;
 import com.playground.spring.petclinic.service.VetService;
+import com.playground.spring.petclinic.service.VisitService;
 
 /**
  * @author bubaibal
@@ -29,49 +31,49 @@ public class DataLoader implements CommandLineRunner {
 	private final VetService vetService;
 	private final PetTypeService petTypeService;
 	private final SpecialityService specialityService;
-	
+	private final VisitService visitService;
 
 	/**
 	 * @param ownerService
 	 * @param vetService
 	 */
-	public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialityService specialityService) {
+	public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService,
+			SpecialityService specialityService, VisitService visitService) {
 		this.ownerService = ownerService;
 		this.vetService = vetService;
 		this.petTypeService = petTypeService;
 		this.specialityService = specialityService;
+		this.visitService = visitService;
 	}
-
-
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		int count = petTypeService.findAll().size();
-		if(count == 0) {
+		if (count == 0) {
 			loadData();
 		}
-		
+
 	}
-	
+
 	private void loadData() {
 		PetType dog = new PetType();
 		dog.setName("Dog");
-		
+
 		PetType savedDogType = petTypeService.save(dog);
-		
+
 		PetType cat = new PetType();
 		cat.setName("Cat");
-		
+
 		PetType savedCatType = petTypeService.save(cat);
-		
+
 		Owner owner1 = new Owner();
 		owner1.setFirstName("Bubai");
 		owner1.setLastName("Bal");
 		owner1.setAddress("Duillya , Andul, Howrah");
 		owner1.setCity("Howrah");
 		owner1.setTelephone("9830540773");
-		
+
 		Pet bubaisDog = new Pet();
 		bubaisDog.setBirthDate(LocalDate.now());
 		bubaisDog.setName("Bosco");
@@ -79,14 +81,14 @@ public class DataLoader implements CommandLineRunner {
 		bubaisDog.setOwner(owner1);
 		owner1.getPets().add(bubaisDog);
 		ownerService.save(owner1);
-		
+
 		Owner owner2 = new Owner();
 		owner2.setFirstName("Soma");
 		owner2.setLastName("Bal");
 		owner2.setAddress("Duillya , Andul, Howrah");
 		owner2.setCity("Howrah");
 		owner2.setTelephone("9051536512");
-		
+
 		Pet somascat = new Pet();
 		somascat.setBirthDate(LocalDate.now());
 		somascat.setName("Meow");
@@ -94,36 +96,44 @@ public class DataLoader implements CommandLineRunner {
 		somascat.setPetType(savedCatType);
 		owner2.getPets().add(somascat);
 		ownerService.save(owner2);
-		
+
 		System.out.println("Loaded Owners.....");
 		
-		Speciality surgery =  new Speciality();
+		Visit catVisit = new Visit();
+		catVisit.setDate(LocalDate.now());
+		catVisit.setPet(somascat);
+		catVisit.setDescription("sneezy kitty");
+		
+		visitService.save(catVisit);
+		System.out.println("Loaded Visits.....");
+
+		Speciality surgery = new Speciality();
 		surgery.setDescription("surgery");
 		Speciality savedSurgery = specialityService.save(surgery);
-		
-		Speciality radiology =  new Speciality();
+
+		Speciality radiology = new Speciality();
 		radiology.setDescription("radiology");
 		Speciality savedRadiology = specialityService.save(radiology);
-		
-		Speciality dentistry =  new Speciality();
+
+		Speciality dentistry = new Speciality();
 		dentistry.setDescription("dentistry");
-		Speciality savedDentistry= specialityService.save(dentistry);
-		
+		Speciality savedDentistry = specialityService.save(dentistry);
+
 		System.out.println("Loaded Specialities.....");
 
 		Vet vet1 = new Vet();
 		vet1.setFirstName("Bidhan");
 		vet1.setLastName("Bal");
 		vet1.getSpecialities().add(savedSurgery);
-		
+
 		vetService.save(vet1);
-		
+
 		Vet vet2 = new Vet();
 		vet2.setFirstName("Sibani");
 		vet2.setLastName("Bal");
 		vet2.getSpecialities().add(savedRadiology);
 		vetService.save(vet2);
-		
+
 		System.out.println("Loaded Vets......");
 	}
 
